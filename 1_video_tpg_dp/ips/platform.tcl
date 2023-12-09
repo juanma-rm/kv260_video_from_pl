814e1fccc2014f2df909ffb79a653de03cd15f01
juanma_rm <juanma@accelerationrobotics.com>
2023-12-09 11:23:25 +0100

    platform updated to include vtpg, vtc and axis2video

--- 1_video_tpg_dp/ips/platform.tcl
+++ 1_video_tpg_dp/ips/platform.tcl
@@ -9,7 +9,7 @@
 ##############################################################################
 
 set PL0_CLK_FREQ_MHZ 100
-set PROJECT_NAME kv260_infrastructure
+set PROJECT_NAME video_tpg_dp
 set BD_TOP bd_top
 set EXTENSIBLE_PLATFORM false
 
@@ -18,7 +18,7 @@ set fan_control_type {ttc0_linux counter_fpga default}
 set FAN_CONTROL "counter_fpga"
 
 ##############################################################################
-# Main block design based on ZUS+ MPSoC + Reset
+# Main block design based on ZUS+ MPSoC
 ##############################################################################
 
 # Create block diagram top
@@ -28,8 +28,8 @@ update_compile_order -fileset sources_1
 # Zynq Ultrscale+ MPSoC block (default preset)
 set zynq_ultra_ps [ create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:3.4 zynq_ultra_ps ]
 apply_bd_automation -rule xilinx.com:bd_rule:zynq_ultra_ps_e -config {apply_board_preset "1" }  $zynq_ultra_ps
-set_property -dict [list \
-    CONFIG.PSU__USE__M_AXI_GP0 {0}                               \
+set_property -dict [ list \
+    CONFIG.PSU__USE__M_AXI_GP0 {1}                               \
     CONFIG.PSU__USE__M_AXI_GP1 {0}                               \
     CONFIG.PSU__USE__M_AXI_GP2 {0}                               \
     CONFIG.PSU__USE__S_AXI_GP0 {0}                               \
@@ -38,13 +38,70 @@ set_property -dict [list \
     CONFIG.PSU__TTC0__PERIPHERAL__ENABLE {1}                     \
     CONFIG.PSU__TTC0__WAVEOUT__ENABLE {1}                        \
     CONFIG.PSU__TTC0__WAVEOUT__IO {EMIO}                         \
+    CONFIG.PSU__USE__VIDEO {1}                                   \
 ] $zynq_ultra_ps
+
+##############################################################################
+# Clocking Wizard block
+##############################################################################
+
+set clk_wiz_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:clk_wiz:6.0 clk_wiz_0 ]
+set_property -dict [ list \
+    CONFIG.CLKOUT1_DRIVES {Buffer} \
+    CONFIG.CLKOUT1_JITTER {97.198} \
+    CONFIG.CLKOUT1_PHASE_ERROR {76.002} \
+    CONFIG.CLKOUT2_DRIVES {Buffer} \
+    CONFIG.CLKOUT2_JITTER {80.036} \
+    CONFIG.CLKOUT2_PHASE_ERROR {76.002} \
+    CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {300.000} \
+    CONFIG.CLKOUT2_USED {true} \
+    CONFIG.CLKOUT3_DRIVES {Buffer} \
+    CONFIG.CLKOUT3_JITTER {90.524} \
+    CONFIG.CLKOUT3_PHASE_ERROR {76.002} \
+    CONFIG.CLKOUT3_REQUESTED_OUT_FREQ {148.5} \
+    CONFIG.CLKOUT3_USED {true} \
+    CONFIG.CLKOUT4_DRIVES {Buffer} \
+    CONFIG.CLKOUT5_DRIVES {Buffer} \
+    CONFIG.CLKOUT6_DRIVES {Buffer} \
+    CONFIG.CLKOUT7_DRIVES {Buffer} \
+    CONFIG.CLK_OUT1_PORT {clk_100M} \
+    CONFIG.CLK_OUT2_PORT {clk_300M} \
+    CONFIG.CLK_OUT3_PORT {clk_148_5M} \
+    CONFIG.FEEDBACK_SOURCE {FDBK_AUTO} \
+    CONFIG.MMCM_BANDWIDTH {OPTIMIZED} \
+    CONFIG.MMCM_CLKFBOUT_MULT_F {14.875} \
+    CONFIG.MMCM_CLKOUT0_DIVIDE_F {14.875} \
+    CONFIG.MMCM_CLKOUT1_DIVIDE {5} \
+    CONFIG.MMCM_CLKOUT2_DIVIDE {10} \
+    CONFIG.MMCM_COMPENSATION {AUTO} \
+    CONFIG.NUM_OUT_CLKS {3} \
+    CONFIG.OPTIMIZE_CLOCKING_STRUCTURE_EN {true} \
+    CONFIG.PRIMITIVE {Auto} \
+    CONFIG.USE_LOCKED {false} \
+    CONFIG.USE_RESET {false} \
+] $clk_wiz_0
 
 ##############################################################################
 # Reset blocks
 ##############################################################################
 
-set ps_reset_pl0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 ps_reset_pl0 ]
+set ps_reset_100M [ create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 ps_reset_100M ]
+set ps_reset_300M [ create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 ps_reset_300M ]
+
+##############################################################################
+# Interrupts (xlconcat)
+##############################################################################
+
+set xlconcat_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:xlconcat:2.1 xlconcat_0 ]
+
+##############################################################################
+# AXI Interconnects
+##############################################################################
+
+set axi_interc_hpm0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_interc_hpm0 ]
+set_property -dict [ list \
+    CONFIG.NUM_MI {2} \
+] $axi_interc_hpm0
 
 ##############################################################################
 # Fan control
@@ -79,21 +136,99 @@ if {$FAN_CONTROL eq "ttc0_linux"} {
 set counter_wrapper [ create_bd_cell -type module -reference counter_wrapper counter_wrapper_inst ]
 
 ##############################################################################
+# Video control: Video TPG + Video Timing Controller + AXI4S to Video Out
+##############################################################################
+
+# Video TPG
+set v_tpg_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:v_tpg:8.2 v_tpg_0 ]
+set_property -dict [ list \
+    CONFIG.MAX_COLS {4096} \
+    CONFIG.MAX_ROWS {2160} \
+] $v_tpg_0
+
+# Video Timing Controller
+set v_tc_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:v_tc:6.2 v_tc_0 ]
+set_property -dict [ list \
+    CONFIG.GEN_F0_VBLANK_HEND {2008} \
+    CONFIG.GEN_F0_VBLANK_HSTART {2008} \
+    CONFIG.GEN_F0_VFRAME_SIZE {1125} \
+    CONFIG.GEN_F0_VSYNC_HEND {2008} \
+    CONFIG.GEN_F0_VSYNC_HSTART {2008} \
+    CONFIG.GEN_F0_VSYNC_VEND {1088} \
+    CONFIG.GEN_F0_VSYNC_VSTART {1083} \
+    CONFIG.GEN_F1_VBLANK_HEND {2008} \
+    CONFIG.GEN_F1_VBLANK_HSTART {2008} \
+    CONFIG.GEN_F1_VFRAME_SIZE {1125} \
+    CONFIG.GEN_F1_VSYNC_HEND {2008} \
+    CONFIG.GEN_F1_VSYNC_HSTART {2008} \
+    CONFIG.GEN_F1_VSYNC_VEND {1088} \
+    CONFIG.GEN_F1_VSYNC_VSTART {1083} \
+    CONFIG.GEN_HACTIVE_SIZE {1920} \
+    CONFIG.GEN_HFRAME_SIZE {2200} \
+    CONFIG.GEN_HSYNC_END {2052} \
+    CONFIG.GEN_HSYNC_START {2008} \
+    CONFIG.GEN_VACTIVE_SIZE {1080} \
+    CONFIG.VIDEO_MODE {1080p} \
+    CONFIG.enable_detection {false} \
+    CONFIG.max_clocks_per_line {8192} \
+] $v_tc_0
+
+# axis4 stream to video out
+set v_axi4s_vid_out_0 [ create_bd_cell -type ip -vlnv xilinx.com:ip:v_axi4s_vid_out:4.0 v_axi4s_vid_out_0 ]
+set_property -dict [ list \
+    CONFIG.C_HAS_ASYNC_CLK {1} \
+    CONFIG.C_NATIVE_COMPONENT_WIDTH {12} \
+] $v_axi4s_vid_out_0
+
+##############################################################################
 # Connections
 ##############################################################################
 
 set pl_clk0 [get_bd_pins $zynq_ultra_ps/pl_clk0]
 set pl_resetn0 [get_bd_pins $zynq_ultra_ps/pl_resetn0]
 
-# ps_reset_pl0
-connect_bd_net $pl_clk0 [get_bd_pins $ps_reset_pl0/slowest_sync_clk]
-connect_bd_net $pl_resetn0 [get_bd_pins $ps_reset_pl0/ext_reset_in]
-set peripheral_reset [get_bd_pins $ps_reset_pl0/peripheral_reset]
+# clk_wiz_0
+connect_bd_net -net zynq_ultra_ps_e_0_pl_clk0 [get_bd_pins clk_wiz_0/clk_in1] $pl_clk0
+connect_bd_net -net clk_wiz_0_clk_100M [get_bd_pins clk_wiz_0/clk_100M] [get_bd_pins axi_interc_hpm0/ACLK] [get_bd_pins axi_interc_hpm0/M00_ACLK] [get_bd_pins axi_interc_hpm0/S00_ACLK] [get_bd_pins ps_reset_100M/slowest_sync_clk] [get_bd_pins v_tc_0/s_axi_aclk] [get_bd_pins zynq_ultra_ps/maxihpm0_fpd_aclk]
+connect_bd_net -net clk_wiz_0_clk_148_5M [get_bd_pins clk_wiz_0/clk_148_5M] [get_bd_pins system_ila_0/clk] [get_bd_pins v_axi4s_vid_out_0/vid_io_out_clk] [get_bd_pins v_tc_0/clk] [get_bd_pins zynq_ultra_ps/dp_video_in_clk]
+connect_bd_net -net clk_wiz_0_clk_300M [get_bd_pins clk_wiz_0/clk_300M] [get_bd_pins axi_interc_hpm0/M01_ACLK] [get_bd_pins ps_reset_300M/slowest_sync_clk] [get_bd_pins system_ila_1/clk] [get_bd_pins v_axi4s_vid_out_0/aclk] [get_bd_pins v_tpg_0/ap_clk]
+
+# ps_reset_100M
+connect_bd_net $pl_resetn0 [get_bd_pins ps_reset_100M/ext_reset_in]
+connect_bd_net -net ps_reset_100M_peripheral_aresetn [get_bd_pins ps_reset_100M/peripheral_aresetn] [get_bd_pins axi_interc_hpm0/ARESETN] [get_bd_pins axi_interc_hpm0/M00_ARESETN] [get_bd_pins axi_interc_hpm0/S00_ARESETN] [get_bd_pins v_tc_0/s_axi_aresetn]
+
+# ps_reset_300M
+connect_bd_net $pl_resetn0 [get_bd_pins ps_reset_300M/ext_reset_in]
+connect_bd_net -net ps_reset_300M_peripheral_aresetn [get_bd_pins ps_reset_300M/peripheral_aresetn] [get_bd_pins axi_interc_hpm0/M01_ARESETN] [get_bd_pins v_axi4s_vid_out_0/aresetn] [get_bd_pins v_tpg_0/ap_rst_n]
+
+# Interrupts
+connect_bd_net -net xlconcat_0_dout [get_bd_pins xlconcat_0/dout] [get_bd_pins zynq_ultra_ps/pl_ps_irq0]
+connect_bd_net -net v_tpg_0_interrupt [get_bd_pins xlconcat_0/In0] [get_bd_pins v_tpg_0/interrupt]
+connect_bd_net -net v_tc_0_irq [get_bd_pins v_tc_0/irq] [get_bd_pins xlconcat_0/In1]
+
+# axi_interc_hpm0
+connect_bd_intf_net -intf_net zynq_ultra_ps_M_AXI_HPM0_FPD [get_bd_intf_pins axi_interc_hpm0/S00_AXI] [get_bd_intf_pins zynq_ultra_ps/M_AXI_HPM0_FPD]
+connect_bd_intf_net -intf_net axi_interc_hpm0_M00_AXI [get_bd_intf_pins axi_interc_hpm0/M00_AXI] [get_bd_intf_pins v_tc_0/ctrl]
+connect_bd_intf_net -intf_net axi_interc_hpm0_M01_AXI [get_bd_intf_pins axi_interc_hpm0/M01_AXI] [get_bd_intf_pins v_tpg_0/s_axi_CTRL]
+
+# Video TPG
+connect_bd_intf_net -intf_net v_tpg_0_m_axis_video [get_bd_intf_pins v_tpg_0/m_axis_video] [get_bd_intf_pins v_axi4s_vid_out_0/video_in] 
+
+# Video Timing Controller
+connect_bd_intf_net -intf_net v_tc_0_vtiming_out [get_bd_intf_pins v_tc_0/vtiming_out] [get_bd_intf_pins v_axi4s_vid_out_0/vtiming_in]
+connect_bd_net -net v_axi4s_vid_out_0_sof_state_out [get_bd_pins v_tc_0/sof_state] [get_bd_pins v_axi4s_vid_out_0/sof_state_out]
+connect_bd_net -net v_axi4s_vid_out_0_vtg_ce [get_bd_pins v_tc_0/gen_clken] [get_bd_pins v_axi4s_vid_out_0/vtg_ce]
+
+# axis4 stream to video out
+connect_bd_net -net v_axi4s_vid_out_0_vid_active_video [get_bd_pins v_axi4s_vid_out_0/vid_active_video] [get_bd_pins zynq_ultra_ps/dp_live_video_in_de]
+connect_bd_net -net v_axi4s_vid_out_0_vid_data [get_bd_pins v_axi4s_vid_out_0/vid_data] [get_bd_pins zynq_ultra_ps/dp_live_video_in_pixel1]
+connect_bd_net -net v_axi4s_vid_out_0_vid_hsync [get_bd_pins v_axi4s_vid_out_0/vid_hsync] [get_bd_pins zynq_ultra_ps/dp_live_video_in_hsync]
+connect_bd_net -net v_axi4s_vid_out_0_vid_vsync [get_bd_pins v_axi4s_vid_out_0/vid_vsync] [get_bd_pins zynq_ultra_ps/dp_live_video_in_vsync]
 
 # counter_wrapper
 create_bd_port -dir O -from 7 -to 0 pmod
-connect_bd_net [get_bd_pins counter_wrapper_inst/clk_i]  [get_bd_pins $pl_clk0]
-connect_bd_net [get_bd_pins counter_wrapper_inst/rst_i]  [get_bd_pins $peripheral_reset]
+connect_bd_net [get_bd_pins counter_wrapper_inst/clk_i]  [get_bd_pins ps_reset_100M/slowest_sync_clk]
+connect_bd_net [get_bd_pins counter_wrapper_inst/rst_i]  [get_bd_pins ps_reset_100M/peripheral_reset]
 connect_bd_net [get_bd_pins counter_wrapper_inst/pmod_o] [get_bd_ports pmod] 
 
 # Fan control
@@ -102,16 +237,23 @@ if {$FAN_CONTROL eq "ttc0_linux"} {
     connect_bd_net [get_bd_pins /xlslice_fan/Dout] [get_bd_ports fan_en_b]
     connect_bd_net [get_bd_pins $zynq_ultra_ps/emio_ttc0_wave_o] [get_bd_pins xlslice_fan/Din]
 } elseif {$FAN_CONTROL eq "counter_fpga"} {
-    connect_bd_net [get_bd_pins pwm_inst/clk_i]  [get_bd_pins ps_reset_pl0/slowest_sync_clk]
-    connect_bd_net [get_bd_pins pwm_inst/rst_i]  [get_bd_pins ps_reset_pl0/peripheral_reset]
-    connect_bd_net [get_bd_pins pwm_inst/pwm_o] [get_bd_pins util_vector_logic_0/Op1]
+    connect_bd_net [get_bd_pins pwm_inst/clk_i]  [get_bd_pins ps_reset_100M/slowest_sync_clk]
+    connect_bd_net [get_bd_pins pwm_inst/rst_i]  [get_bd_pins ps_reset_100M/peripheral_reset]
     create_bd_port -dir O -from 0 -to 0 fan_en_b
+    connect_bd_net [get_bd_pins pwm_inst/pwm_o] [get_bd_pins util_vector_logic_0/Op1]
     connect_bd_net [get_bd_ports fan_en_b]      [get_bd_pins util_vector_logic_0/Res]
     connect_bd_net [get_bd_pins xlconstant_0/dout] [get_bd_pins pwm_inst/duty_cycle_in]
 } elseif {[lsearch -exact $fan_control_type $FAN_CONTROL] == -1} {
     puts "Error: FAN_CONTROL must be one of {ttc0_linux counter_fpga default}"
     exit 1
 }
+
+##############################################################################
+# AXI address mapping
+##############################################################################
+
+assign_bd_address -offset 0xA0000000 -range 0x00010000 -target_address_space [get_bd_addr_spaces zynq_ultra_ps/Data] [get_bd_addr_segs v_tc_0/ctrl/Reg] -force
+assign_bd_address -offset 0xA0010000 -range 0x00010000 -target_address_space [get_bd_addr_spaces zynq_ultra_ps/Data] [get_bd_addr_segs v_tpg_0/s_axi_CTRL/Reg] -force
 
 ##############################################################################
 # Regenerate layout and validate design
